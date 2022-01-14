ffb7429420d05c101b33b2b9502e06524e6d50c4
pang yew ken <ukenster@gmail.com>
2022-01-15 02:02:57 +0800

    Wrote 2 tests for state.rb to check state_of_the_game method

--- lib/state.rb
+++ lib/state.rb
@@ -1,9 +1,20 @@
 require_relative 'game'
 
-def state_of_the_game
+def state_of_the_game(player_one_choice, player_two_choice, mode)
 # return a message containing player 1's and player 2's states.
+  case mode
+  when 1
+    player_one = "Your choice"
+    player_two = "PC choice"
+  when 2
+    player_one = "PC#1 choice"
+    player_two = "PC#2 choice"
+  else
+
+  end
+  return "#{player_one} is #{player_one_choice}, #{player_two} is #{player_two_choice}"
 end
 
-def end_game_message
+def end_game_message(player_one_choice, player_two_choice)
 # return a message telling if player 1 or 2 won.
 end
--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -1,1 +1,22 @@
 require 'state'
+
+describe "#state_of_the_game" do
+  it "should return a choice as string" do
+    expect(state_of_the_game('rock', 'paper', 1)).to be_a String
+  end
+
+  it "should return an interpolated String with player one and two choices" do
+    expect(state_of_the_game('rock', 'paper', 1)).to match(/Your choice is rock/i)
+    expect(state_of_the_game('rock', 'paper', 1)).to match(/PC choice is paper/i)
+  end
+end
+
+# describe "#end_of_the_game" do
+#   it "should return a choice as string" do
+#     expect(random_choice).to be_a String
+#   end
+
+#   it "should return either rock paper or scissors" do
+#     expect(%w[rock paper scissors]).to include (random_choice)
+#   end
+# end
