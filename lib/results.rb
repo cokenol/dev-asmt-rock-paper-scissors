cdb929a2b88bc5f557f916f7f43e8bc6a9228376
pang yew ken <ukenster@gmail.com>
2022-01-15 16:12:39 +0800

    Created the rock-paper-scissors hash. and end_of_the_game result

---
+++ lib/results.rb
@@ -0,0 +1,29 @@
+module Result
+  HASH = {
+    rock: {
+      scissors: 'win',
+      paper: 'lost',
+      rock: 'draw'
+    },
+    paper: {
+      rock: 'win',
+      scissors: 'lost',
+      paper: 'draw'
+    },
+    scissors: {
+      paper: 'win',
+      rock: 'lost',
+      scissors: 'draw'
+    }
+  }
+end
+# Scissors cuts Paper
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitates Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
+# Rock crushes Scissors
--- lib/state.rb
+++ lib/state.rb
@@ -1,4 +1,5 @@
 require_relative 'game'
+require_relative 'results'
 
 def state_of_the_game(player_one_choice, player_two_choice, mode)
 # return a message containing player 1's and player 2's states.
@@ -15,6 +16,19 @@ def state_of_the_game(player_one_choice, player_two_choice, mode)
   return "#{player_one} is #{player_one_choice}, #{player_two} is #{player_two_choice}"
 end
 
-def end_game_message(player_one_choice, player_two_choice)
+def end_of_the_game(player_one_choice, player_two_choice, mode)
 # return a message telling if player 1 or 2 won.
+# return a message containing player 1's and player 2's states.
+  case mode
+  when 1
+    player_one = "You"
+    player_two = "PC"
+  when 2
+    player_one = "PC#1"
+    player_two = "PC#2"
+  else
+
+  end
+  result = Result::HASH[player_one_choice.to_sym][player_two_choice.to_sym]
+  return "#{player_one} #{result}!"
 end
