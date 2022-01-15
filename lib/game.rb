5799cb030925de47b2ca80f0f48c3fcf9654cca6
pang yew ken <ukenster@gmail.com>
2022-01-15 12:28:16 +0800

    Wrote test for end_of_the_game to return correct outcome

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,4 @@
 def random_choice
   # Return a random choice of rock, paper or scissors
-  return %w[rock paper scissors].sample
+  %w[rock paper scissors].sample
 end
--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -1,22 +1,30 @@
 require 'state'
 
-describe "#state_of_the_game" do
-  it "should return a choice as string" do
+describe '#state_of_the_game' do
+  it 'should return a choice as string' do
     expect(state_of_the_game('rock', 'paper', 1)).to be_a String
   end
 
-  it "should return an interpolated String with player one and two choices" do
+  it 'should return an interpolated String with player one and two choices' do
     expect(state_of_the_game('rock', 'paper', 1)).to match(/Your choice is rock/i)
     expect(state_of_the_game('rock', 'paper', 1)).to match(/PC choice is paper/i)
   end
 end
 
-# describe "#end_of_the_game" do
-#   it "should return a choice as string" do
-#     expect(random_choice).to be_a String
-#   end
+describe '#end_of_the_game' do
+  it 'should return a choice as string' do
+    expect(end_of_the_game('rock', 'paper', 1)).to be_a String
+  end
 
-#   it "should return either rock paper or scissors" do
-#     expect(%w[rock paper scissors]).to include (random_choice)
-#   end
-# end
+  it 'should return the correct outcome' do
+    expect(end_of_the_game('rock', 'paper', 1)).to match(/You lost!/i)
+    expect(end_of_the_game('rock', 'scissors', 1)).to match(/You win!/i)
+    expect(end_of_the_game('rock', 'rock', 1)).to match(/Draw/i)
+    expect(end_of_the_game('paper', 'scissors', 1)).to match(/You lost!/i)
+    expect(end_of_the_game('paper', 'rock', 1)).to match(/You win!/i)
+    expect(end_of_the_game('paper', 'paper', 1)).to match(/Draw/i)
+    expect(end_of_the_game('scissors', 'rock', 1)).to match(/You lost!/i)
+    expect(end_of_the_game('scissors', 'paper', 1)).to match(/You win!/i)
+    expect(end_of_the_game('scissors', 'scissors', 1)).to match(/Draw/i)
+  end
+end
