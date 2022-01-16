0ff03c21d7528ad3964997528f3c33370a5a0731
pang yew ken <ukenster@gmail.com>
2022-01-16 14:37:53 +0800

    Psuedo coded interface flows documentation

--- lib/interface.rb
+++ lib/interface.rb
@@ -5,7 +5,6 @@ require_relative 'state'
 require_relative 'game'
 
 def choose_mode
-  # Objective
   continue = true
   while continue
     puts '=========================================='
@@ -22,6 +21,7 @@ def choose_mode
 end
 
 def player_choose
+  clear
   puts '========================'
   puts 'Rock, paper or scissors?'
   puts '1. Rock'
@@ -37,21 +37,36 @@ def clear
   print `clear`
 end
 
-continue = true
+# Clear terminal once ruby interface.rb is run
 clear
+
+# First user prompt to choose a mode.
 mode = choose_mode
+continue = [1, 2].include?(mode)
+
+# Clear terminal after mode is chosen
 clear
-while continue && [1, 2].include?(mode)
+
+# Loop to prompt user to choose a mode until 1 or 2 is not chosen
+while continue
+  # When 1. Player vs PC is chosen. Prompt user to choose rock, paper or scissors.
   player_one_choice = player_choose if mode == 1
+
+  # When 2. PC vs PC is chosen. Player one choice is randomly chosen.
   player_one_choice = random_choice if mode == 2
   clear
+
+  # Player two is default PC so the choice is always random.
   player_two_choice = random_choice
+
+  # Prints the choices and result of the game.
   puts state_of_the_game(player_one_choice, player_two_choice, mode)
   puts end_of_the_game(player_one_choice, player_two_choice, mode)
+
+  # Prompts users to choose mode again after the game is done.
   mode = choose_mode
   continue = [1, 2].include?(mode)
 end
 
 puts 'Exiting game. Bye. See you again.'
 # Each new game should be a different game.
-# The game is a command line application.
