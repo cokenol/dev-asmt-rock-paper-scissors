a3f018e747de1e65d1a2c2b9604ed470db0ab127
pang yew ken <ukenster@gmail.com>
2022-01-18 21:48:17 +0800

    renamed results.rb to result.rb

--- lib/result.rb
+++ lib/result.rb
@@ -1,13 +1,10 @@
 class Result
-  attr_reader :rule_set
-
-  def initialize
-    @rule_set =
-      {
-        rock: [:scissors],
-        paper: [:rock],
-        scissors: [:paper]
-      }
+  def rule_set
+    {
+      rock: [:scissors],
+      paper: [:rock],
+      scissors: [:paper]
+    }
   end
 
   def outcome(player_one_choice, player_two_choice)
@@ -16,7 +13,7 @@ class Result
     elsif rule_set[player_one_choice.to_sym].include?(player_two_choice.to_sym)
       'win'
     else
-      'lose'
+      'lost'
     end
   end
 end
