1ee5ad67b463f993b999d780cc3b8c6d145e4c6c
pang yew ken <ukenster@gmail.com>
2022-01-20 23:45:38 +0800

    refactored test so codes do not repeat

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -13,38 +13,26 @@ end
 
 RSpec.describe Interface do
   describe '#choose_mode' do
+    output = StringIO.new
+    $stdout = output
+    input = StringIO.new("1\n")
+    $stdin = input
+    console_interface = Interface.new(input: input, output: output)
+    console_interface.choose_mode
+
     it 'sends a prompt of Player vs PC mode to choose' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.choose_mode
       expect(output.string).to include('Player vs PC')
     end
 
     it 'sends a prompt of PC vs PC mode to choose' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.choose_mode
       expect(output.string).to include('PC vs PC')
     end
 
     it 'sends a prompt of exit mode to choose' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.choose_mode
       expect(output.string).to include('Exit')
     end
 
     it 'should have an input for choosing modes' do
-      input = StringIO.new("1\n")
-      $stdin = input
-      console_interface = Interface.new(input: input)
-      console_interface.choose_mode
       expect(input.string).to include("1\n")
     end
 
@@ -63,59 +51,34 @@ RSpec.describe Interface do
 
   describe '#player_choose' do
     choices = %i[rock paper scissors lizard spock]
+    output = StringIO.new
+    $stdout = output
+    input = StringIO.new("1\n")
+    $stdin = input
+    console_interface = Interface.new(input: input, output: output)
+    console_interface.player_choose(choices)
 
     it 'should display rock as a choice in terminal' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.player_choose(choices)
       expect(output.string).to include('rock')
     end
 
     it 'should display paper as a choice in terminal' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.player_choose(choices)
       expect(output.string).to include('paper')
     end
 
     it 'should display scissors as a choice in terminal' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.player_choose(choices)
       expect(output.string).to include('scissors')
     end
 
     it 'should display lizard as a choice in terminal' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.player_choose(choices)
       expect(output.string).to include('lizard')
     end
 
     it 'should display spock as a choice in terminal' do
-      output = StringIO.new
-      $stdout = output
-      $stdin = StringIO.new("1\n")
-      console_interface = Interface.new(output: output)
-      console_interface.player_choose(choices)
       expect(output.string).to include('spock')
     end
 
     it 'should have an input for choosing choices' do
-      output = StringIO.new
-      $stdout = output
-      input = StringIO.new("1\n")
-      $stdin = input
-      console_interface = Interface.new(input: input, output: output)
-      console_interface.player_choose(choices)
       expect(input.string).to include("1\n")
     end
   end
