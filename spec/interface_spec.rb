0a09b51edff453712fa2eb9685a401c27635a7a3
pang yew ken <ukenster@gmail.com>
2022-01-20 17:16:19 +0800

    succesfully written test for gets and puts with no prompt

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -4,16 +4,36 @@ require 'pry-byebug'
 RSpec.describe Interface do
 
   describe '#choose_mode' do
-    it 'sends a prompt question to output' do
+    it 'sends a prompt of Player vs PC mode to choose' do
       output = StringIO.new
+      $stdout = output
+      $stdin = StringIO.new("1\n")
       console_interface = Interface.new(output: output)
       console_interface.choose_mode
       expect(output.string).to include('Player vs PC')
-      # expect(output.string).to include("Exit")
     end
 
-    it 'should have an input' do
+    it 'sends a prompt of PC vs PC mode to choose' do
+      output = StringIO.new
+      $stdout = output
+      $stdin = StringIO.new("1\n")
+      console_interface = Interface.new(output: output)
+      console_interface.choose_mode
+      expect(output.string).to include('PC vs PC')
+    end
+
+    it 'sends a prompt of exit mode to choose' do
+      output = StringIO.new
+      $stdout = output
+      $stdin = StringIO.new("1\n")
+      console_interface = Interface.new(output: output)
+      console_interface.choose_mode
+      expect(output.string).to include('Exit')
+    end
+
+    it 'should have an input for choosing modes' do
       input = StringIO.new("1\n")
+      $stdin = input
       console_interface = Interface.new(input: input)
       console_interface.choose_mode
 
