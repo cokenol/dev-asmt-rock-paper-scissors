da1a7837f672583c3a5096226fc13c822413af0f
pang yew ken <ukenster@gmail.com>
2022-01-21 10:05:14 +0800

    renamed get_input. move input instructions to top

--- lib/view.rb
+++ lib/view.rb
@@ -1,33 +1,32 @@
-# require 'pry-byebug'
 # View class to define methods on display, getting input and display errors.
 class View
   # Display array of choices in terminal
   def display(choices)
     puts '=========================================='
-    choices.each_with_index do |choice, index|
-      puts "#{index + 1}. #{choice}"
-    end
     print 'Input '
     choices.each_with_index do |choice, index|
       print choices.count == (index + 1) ? "or #{index + 1}" : "#{index + 1}, "
     end
     puts ''
+    choices.each_with_index do |choice, index|
+      puts "#{index + 1}. #{choice}"
+    end
     puts '=========================================='
   end
 
   # Get user input after display
-  def get_input
+  def ask_input
     gets.chomp.to_i
   end
 
   # Display error when input is not within available options.
   def display_error(choice, choices)
-    unless choices.include?(choice)
-      clear
-      puts '=========================================='
-      puts "ERROR: #{choice} is not a valid option."
-      puts 'Please choose one of the following:'
-    end
+    return '' if choices.include?(choice)
+
+    clear
+    puts '=========================================='
+    puts "ERROR: #{choice} is not a valid option."
+    puts 'Please choose one of the following:'
   end
 
   # Method to clear terminal
